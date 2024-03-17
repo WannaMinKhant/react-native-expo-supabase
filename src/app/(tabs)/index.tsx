fe1ad34c4c1ce644542cfe3a6742d5d1d91cfc85
WannaMinKhant <wannakyawhitn17@gmail.com>
2024-03-17 23:56:05 +0300

    Details screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,5 @@
-import { StyleSheet, Text, View, Image, FlatList } from "react-native";
-import Colors from "@/src/constants/Colors";
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
+import { Redirect } from "expo-router";
 
-const product = products[1];
-
-export default function MenuScreen() {
-  return (
-      <FlatList 
-      data={products}
-      renderItem={({item}) =><ProductListItem product={item}/> }
-      numColumns={2}
-      contentContainerStyle={{ gap: 10, padding: 10}}
-      columnWrapperStyle={{ gap: 10 }}
-      />
-
-  );
+export default function TabIndex(){
+    return <Redirect href={'/menu/'}/>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-});
