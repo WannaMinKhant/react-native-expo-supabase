4a6d9d9b2ac7bf10c1e577cbfebda9a7e198f8d5
WannaMinKhant <wannakyawhitn17@gmail.com>
2024-03-21 17:15:25 +0300

    Create Proudct Form with Validation and  Image Picker

---
+++ src/app/(admin)/menu/create.tsx
@@ -0,0 +1,118 @@
+import { StyleSheet, Text, TextInput, View, Image } from "react-native";
+import React, { useState } from "react";
+import Button from "@/src/components/Button";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
+
+const CreateProductScreen = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [error, setError] = useState("");
+  const [image, setImage] = useState<String | null>(null);
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const validateInput = () => {
+    if (!name) {
+      setError("Name is required");
+      return false;
+    }
+
+    if (!price) {
+      setError("Price is required");
+      return false;
+    }
+
+    if (isNaN(parseFloat(price))) {
+      setError("Price is not a number");
+      return false;
+    }
+    return true;
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const onCreate = () => {
+    if (!validateInput()) {
+      return;
+    }
+    console.warn("Creating product");
+    resetFields();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        placeholder="Name"
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+      />
+      <Text style={styles.label}>Price ($)</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+      <Text style={{ color: "red" }}>{error}</Text>
+      <Button onPress={onCreate} text="Create" />
+    </View>
+  );
+};
+
+export default CreateProductScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  label: {
+    color: "gray",
+    fontSize: 16,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+});
